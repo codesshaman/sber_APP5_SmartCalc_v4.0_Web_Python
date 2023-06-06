6fce888ea6951deb04f17297a46d88cc6c11dd9d
codesshaman <nicksemail@yandex.ru>
2023-06-06 14:54:12 +0300

    work calc

--- src/smartcalc_v5/apps/model/views.py
+++ src/smartcalc_v5/apps/model/views.py
@@ -1,7 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
-# Create your views here.
-def calc(request):
-    return render(request, 'index.html')
+def calculator(request):
+    return render(request, 'calculator.html')
+
+
+def calculate(request):
+    if request.method == 'POST':
+        num1 = float(request.POST['num1'])
+        num2 = float(request.POST['num2'])
+        operator = request.POST['operator']
+
+        result = 0
+
+        if operator == '+':
+            result = num1 + num2
+        elif operator == '-':
+            result = num1 - num2
+        elif operator == '*':
+            result = num1 * num2
+        elif operator == '/':
+            result = num1 / num2
+
+        return render(request, 'calculator.html', {'result': result})
+    else:
+        return render(request, 'calculator.html')
