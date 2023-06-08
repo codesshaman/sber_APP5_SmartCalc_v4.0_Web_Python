b338e44a251e7e96d330c046ce81d027b2f48e05
codesshaman <nicksemail@yandex.ru>
2023-06-08 11:31:21 +0300

    work input

--- src/smartcalc_v5/apps/model/urls.py
+++ src/smartcalc_v5/apps/model/urls.py
@@ -2,7 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.calculator, name='calculator'),
-    path('calculate', views.calculate, name='calculate'),
-    path('calc', views.calc, name='calc')
+    path('', views.calc, name='calc')
 ]
