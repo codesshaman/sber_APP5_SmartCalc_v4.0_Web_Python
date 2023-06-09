c7e6d6ca38647d8a838f4ac8b3a66cf98a8969e2
codesshaman <nicksemail@yandex.ru>
2023-06-09 17:01:04 +0300

    last changes

--- src/smartcalc_v5/apps/model/mathfunc.py
+++ src/smartcalc_v5/apps/model/mathfunc.py
@@ -1,11 +1,24 @@
-import math
 import re
-
+from core import SmartCalc as sc
 
 def parse_expression(expression):
     # Используем регулярное выражение для разделения строки на числа, операции и скобки
-    tokens = re.findall(r'[\d.]+|\+|\-|\*|/|\(|\)', expression)
+    pattern = r"(\s*sin\s+" \
+              r"|\s*cos\s+" \
+              r"|\s*acos\s+" \
+              r"|\s*asin\s+" \
+              r"|\s*ln\s+" \
+              r"|\s*log\s+" \
+              r"|\s*tan\s+" \
+              r"|\s*atan\s+" \
+              r"|\s*sqrt\s+" \
+              r"|[\d.]+|\+|\-|\*|/|\(|\))"
+    tokens = re.findall(pattern, expression)
 
+    # Формируем словарь токенов
+    dict = ('+', '-', '*', '/',
+            'sin ', 'cos ', 'acos ', 'asin ',
+            'ln ', 'log ', 'tan ', 'atan ', 'sqrt ')
     # Создаём пустой стек
     stack = []
 
@@ -20,15 +33,18 @@ def parse_expression(expression):
         if token.isdigit() or '.' in token:
             # Если токен является числом, добавляем его в выходной список
             output.append(float(token))
-        elif token in ('+', '-', '*', '/'):
+        elif token.strip() in dict:
             # Если токен является операцией
             while stack and stack[-1] != '(' and precedence[token] <= precedence.get(stack[-1], 0):
                 # Проверяем приоритет текущей операции относительно операций в стеке
                 output.append(stack.pop())
-            stack.append(token)
+            stack.append(token.strip())
+        elif token in dict:
+            # Если токен есть в словаре токенов, добавляем его в стек
+            stack.append(token.strip())
         elif token == '(':
             # Если токен является открывающей скобкой, добавляем его в стек
-            stack.append(token)
+            stack.append(token.strip())
         elif token == ')':
             # Если токен является закрывающей скобкой
             while stack and stack[-1] != '(':
@@ -47,11 +63,14 @@ def parse_expression(expression):
 
 def evaluate_expression(tokens):
     stack = []
+    dict = ('+', '-', '*', '/',
+            'sin ', 'cos ', 'acos ', 'asin ',
+            'ln ', 'log ', 'tan ', 'atan ', 'sqrt ')
 
     for token in tokens:
         if isinstance(token, float):
             stack.append(token)
-        elif token in ('+', '-', '*', '/'):
+        elif token in dict:
             operand2 = stack.pop()
             operand1 = stack.pop()
             if token == '-':
@@ -61,6 +80,24 @@ def evaluate_expression(tokens):
             elif token == '*':
                 result = operand1 * operand2
             elif token == '/':
+                result = operand1 / operand2
+            elif token == 'cos':
+                result = sc.math_cos(operand1)
+            elif token == 'sin':
+                result = operand1 / operand2
+            elif token == 'acos':
+                result = operand1 / operand2
+            elif token == 'asin':
+                result = operand1 / operand2
+            elif token == 'ln':
+                result = operand1 / operand2
+            elif token == 'log':
+                result = operand1 / operand2
+            elif token == 'tan':
+                result = operand1 / operand2
+            elif token == 'atan':
+                result = operand1 / operand2
+            elif token == 'sqrt':
                 result = operand1 / operand2
             stack.append(result)
 
@@ -76,7 +113,7 @@ def evaluate_expression(tokens):
 #         return value
 
 def main():
-    expression = "(3 + 4)"
+    expression = "cos 0.333"
     parsed_tokens = parse_expression(expression)
     result = evaluate_expression(parsed_tokens)
     # result = result_display(result)
