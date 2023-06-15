46df5d0f5dd6d8850cd3f0b6e5a4fe7de854be0e
codesshaman <nicksemail@yandex.ru>
2023-06-15 21:17:03 +0300

    add graph

--- src/smartcalc_v5/smartcalc_v5/urls.py
+++ src/smartcalc_v5/smartcalc_v5/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.urls import include, path
 from django.contrib import admin
-from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('apps.model.urls'))
+    path('', include('apps.model.urls')),
+    path('graph/', include('apps.graph.urls')),
 ]
